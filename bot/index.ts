262a0dc17000e92f20ea6225ead07fe5257bc6a4
aburavov <andrey.vip.andrey@mail.ru>
2025-02-13 12:08:13 +0500

    first commit

--- bot/index.ts
+++ bot/index.ts
@@ -1,10 +1,7 @@
 import {Telegraf} from "telegraf";
 import {message} from "telegraf/filters";
 
-console.log(process.env.BOT_TOKEN)
-
-// const bot = new Telegraf(process.env.BOT_TOKEN)
-const bot = new Telegraf("7525301982:AAG3cE_CTO4zYbOg3zqd1PdyERXancg2zWo");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.launch();
 
