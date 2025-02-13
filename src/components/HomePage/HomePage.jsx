f75cb93c81e9887097838ff5188e9d0b89ffb308
aburavov <andrey.vip.andrey@mail.ru>
2025-02-13 13:33:52 +0500

    main page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,67 +3,59 @@ import { Link } from 'react-router-dom';
 
 export default function HomePage() {
     return (
-        <div className='app-container'>
-            <div className='text-center mb-3 bg-success text-white rounded fs-4 fw-semibold'>
-                <p>Откройте для себя Тайланд с нашим удобным гидом!</p>
-            </div>
-            <div className="d-flex gap-2 justify-content-between">
-                <Link to="/house" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text">Недвижимость</p>
-                    <div className='position-absolute bottom-0 end-0'>
-                        <img src='/svg/house.svg' alt="Недвижимость"/>
-                    </div>
-                </Link>
-                <Link to="/transport" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text">Аренда транспорта</p>
-                    <div className='position-absolute bottom-0 end-0'>
-                        <img src='/svg/scooter.svg' alt="Аренда транспорта"/>
-                    </div>
-                </Link>
+        <>
+            <div className='background_main h-100 w-100 position-absolute'></div>
+            <div className='app-container h-100 w-100 z-1'>
+                <div className='text-center mb-3 rounded fs-4 fw-semibold'>
+                    <p className='header'>THAILAND</p>
+                </div>
+                <div className="d-flex gap-2 justify-content-between mb-2">
+                    <Link to="/house" className="button rounded d-flex position-relative bg-estate">
+                        <img src='/public/img/image_estate.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>недвижимость</p>
+                    </Link>
+                    <Link to="/transport" className="button rounded d-flex position-relative">
+                        <img src='/public/img/image_transport.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>аренда транспорта</p>
+                    </Link>
 
-            </div>
-            <div className="d-flex gap-2 justify-content-between">
-                <Link to="/visa" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text m-0">Визы</p>
-                    <div className="position-absolute bottom-0 end-0">
-                        <img src='/svg/visa.svg' alt="Визы"/>
-                    </div>
-                </Link>
-                <Link to="/cards" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text">Карты и права</p>
-                    <div className='position-absolute bottom-0 end-0'>
-                        <img src='/svg/cards.svg' alt="Карты и права"/>
-                    </div>
-                </Link>
-            </div>
-            <div className="d-flex gap-2 justify-content-between">
-                <Link to="/services" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text m-0">Услуги</p>
-                    <div className="position-absolute bottom-0 end-0">
-                        <img src='/svg/uslugi.svg' alt="Услуги"/>
-                    </div>
-                </Link>
-                <Link to="/excursions" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text">Туры и экскурсии</p>
-                    <div className='position-absolute bottom-0 end-0'>
-                        <img src='/svg/excursion.svg' alt="Туры и экскурсии"/>
-                    </div>
-                </Link>
-            </div>
-            <div className="d-flex gap-2 justify-content-between">
-                <Link to="/tour" className="block rounded w-100 d-flex position-relative text-decoration-none">
-                    <p className="description-text m-0">Тур в Тай</p>
-                    <div className="position-absolute bottom-0 end-0">
-                        <img src='/svg/tour.svg' alt="Тур в Тай"/>
-                    </div>
-                </Link>
-                <div className="block rounded w-100 d-flex position-relative">
-                    <p className="description-text m-0">Общий чат</p>
-                    <div className="position-absolute bottom-0 end-0">
-                        <img src='/svg/chat.svg' alt="Общий чат"/>
+                </div>
+                <div className="d-flex gap-2 justify-content-between mb-2">
+                    <Link to="/visa" className="button rounded d-flex position-relative">
+                        <img src='/public/img/image_visa.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>визы</p>
+                    </Link>
+                    <Link to="/cards" className="button rounded d-flex position-relative">
+                        <img src='/public/img/image_cards.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>карты и права</p>
+                    </Link>
+                </div>
+                <div className="d-flex gap-2 justify-content-between mb-2">
+                    <Link to="/services" className="button rounded d-flex position-relative">
+                        <img src='/public/img/image_services.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>услуги</p>
+                    </Link>
+                    <Link to="/excursions" className="button rounded d-flex position-relative">
+                        <img src='/public/img/image_ecsoursions.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>туры и экскурсии</p>
+                    </Link>
+                </div>
+                <div className="d-flex gap-2 justify-content-between">
+                    <Link to="/tour" className="button rounded d-flex position-relative">
+                        <img src='/public/img/image_tour_thai.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>тур в тай</p>
+                    </Link>
+                    <div className='button position-relative'>
+                        <img src='/public/img/image_chat.png' className='w-100 h-100'/>
+                        <p className='button-text text-white m-0 p-0 lh-1 position-absolute bottom-0'>общий чат</p>
                     </div>
                 </div>
+                <div className='w-100 mt-3'>
+                    <p className='lh-1 footer'>Таиланд — это место, где мечты становятся реальностью.</p>
+                </div>
+
             </div>
-        </div>
+        </>
+
     );
 }
